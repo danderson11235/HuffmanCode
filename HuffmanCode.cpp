e5a94aacfaea6eaeaef6bf940494afda20c41dce
danderson11235 <danderson11235@gmail.com>
2021-10-08 19:50:15 -0400

    Beginning work on the cpp version

--- HuffmanCode.cpp
+++ HuffmanCode.cpp
@@ -1,9 +1,30 @@
 #include "HuffmanCode.hpp"
+#include <map>
+#include <stdio.h>
 
-HuffmanCode::HuffmanCode(/* args */)
+HuffmanCode::HuffmanCode()
 {
+    root = nullptr;
 }
 
 HuffmanCode::~HuffmanCode()
 {
+}
+
+bool HuffmanCode::encode(std::string fileName) {
+    FILE* fi = fopen(fileName.c_str(), "r+");
+    char c;
+    std::map<char, int> freqMap;
+    while ((c = getc(fi)) != EOF)
+    {
+        freqMap[c]++;
+    }
+    
+
+}
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
 }
--- HuffmanCode.hpp
+++ HuffmanCode.hpp
@@ -12,10 +12,10 @@ private:
         Node(char _item, Node* _zero, Node* _one) : 
             item(_item), zero(_zero), one(_one) {}
     };
-    
+    Node* root;
 public:
-    HuffmanCode(std::string fileName);
+    HuffmanCode();
     ~HuffmanCode();
-    bool encode();
-    bool decode();
+    bool encode(std::string fileName);
+    bool decode(std::string fileName);
 };
