6f996814868bd85fbe2ab5c4c7da0b4c6ce2b833
danderson11235 <danderson11235@gmail.com>
2022-05-24 19:51:52 -0400

    added the functionality to create the tree in HC.cpp

--- HuffmanCode.cpp
+++ HuffmanCode.cpp
@@ -1,10 +1,10 @@
 #include "HuffmanCode.hpp"
-#include <map>
 #include <stdio.h>
 
-HuffmanCode::HuffmanCode()
+HuffmanCode::HuffmanCode(std::string fileName)
 {
     root = nullptr;
+    // charList = getCharList(fileName);
 }
 
 HuffmanCode::~HuffmanCode()
@@ -20,7 +20,58 @@ bool HuffmanCode::encode(std::string fileName) {
         freqMap[c]++;
     }
     
+    std::map<char, int>::iterator map_it;
+    for (map_it = freqMap.begin(); map_it != freqMap.end(); map_it++) {
+        nodeList.push_back(new Node(map_it->first, map_it->second));
+    }
+    std::vector<Node*>::iterator vec_it;
+    std::vector<Node*>::iterator vec_save;
+    while (!nodeList.empty())
+    {
+        if (nodeList.size() == 1) {
+            root = nodeList[0];
+            nodeList.pop_back();
+            break;
+        }
+        Node* temp = nodeList[0];
+        int min = temp->count;
+        for (vec_it = nodeList.begin(); vec_it != nodeList.end(); vec_it++) {
+            if ((*vec_it)->count < min) {
+                temp = *vec_it;
+                min = temp->count;
+                vec_save = vec_it;
+            }
+        }
+        nodeList.erase(vec_save);
+        // Do it again
+        Node* temp2 = nodeList[0];
+        min = temp2->count;
+        for (vec_it = nodeList.begin(); vec_it != nodeList.end(); vec_it++) {
+            if ((*vec_it)->count < min) {
+                temp = *vec_it;
+                min = temp->count;
+                vec_save = vec_it;
+            }
+        }
+        nodeList.erase(vec_save);
 
+        Node* comb;
+        if (temp->count > temp2->count) comb = new Node(0, temp->count + temp2->count, temp2, temp);
+        else comb = new Node(0, temp->count + temp2->count, temp, temp2);
+        
+        nodeList.push_back(comb);
+    }
+    
+
+}
+
+std::vector<char> HuffmanCode::getCharList(std::string fileName) {
+    FILE* fi = fopen(fileName.c_str(), "r+");
+    char c;
+    std::map<char, int> freqMap;
+    while ((c = getc(fi)) != EOF) {
+        freqMap[c]++;
+    }
 }
 
 int main(int argc, char const *argv[])
--- HuffmanCode.hpp
+++ HuffmanCode.hpp
@@ -1,5 +1,7 @@
 #pragma once
 #include <string>
+#include <map>
+#include <vector>
 
 class HuffmanCode
 {
@@ -7,14 +9,19 @@ private:
     struct Node
     {
         char item;
+        int count;
         Node* zero;
         Node* one;
-        Node(char _item, Node* _zero, Node* _one) : 
-            item(_item), zero(_zero), one(_one) {}
+        Node(char _item, int _count, Node* _zero, Node* _one) : 
+            item(_item), count(_count), zero(nullptr), one(nullptr) {}
+        Node(char _item, int _count) :
+            item(_item), count(_count), zero(nullptr), one(nullptr) {}
     };
     Node* root;
+    std::vector<char> getCharList(std::string fileName);
+    std::vector<Node*> nodeList;
 public:
-    HuffmanCode();
+    HuffmanCode(std::string fileName);
     ~HuffmanCode();
     bool encode(std::string fileName);
     bool decode(std::string fileName);
